5777ae80c1d4ff1b2cc5a7347b18168eda1e1871
Dalai Felinto <dfelinto@gmail.com>
2017-05-07 21:25:19 +0200

    Fixing errors
    And leaving it forever in the internet.

--- src/operator.py
+++ src/operator.py
@@ -11,8 +11,10 @@ from bpy.types import Operator
 # ############################################################
 
 class Strip(object):
-    def __init__(self, strip, index):
+    def __init__(self, index, strip, fps, fps_base):
         self._index = index
+        self._fps = fps
+        self._fps_base = fps_base
         self._process_strip(strip)
         self._create_data_dictionary()
 
@@ -21,34 +23,54 @@ class Strip(object):
         Export strip data into .edl formatted line
         """
         line = "{index:03d} " \
-               "{reel_name:-8s} " \
+               "{reel_name:8s} " \
                "{channel_type} " \
                "{cut_type} " \
                "{source_in} " \
                "{source_out} " \
                "{edit_in} " \
-               "{edit_out} " \
+               "{edit_out}" \
                "\n" \
-               .format(self._data)
+               .format(**self._data)
         return line
 
     def _create_data_dictionary(self):
         self._data = {
-            "index": self._index,
+            "index": self._index + 1,
             "reel_name": self._reel_name,
             "channel_type": self._channel_type,
             "cut_type": "C",
-            "source_in": self._source_in,
-            "source_out": self._source_out,
-            "edit_in": self._edit_in,
-            "edit_out": self._edit_out,
+            "source_in": self._smpte(self._source_in),
+            "source_out": self._smpte(self._source_out),
+            "edit_in": self._smpte(self._edit_in, style='RESOLVE'),
+            "edit_out": self._smpte(self._edit_out, style='RESOLVE'),
             }
+
+    def _smpte(self, frame, style=None):
+        """
+        Convert frames to SMPTE
+        """
+        if style is None:
+            pass
+
+        elif style == 'RESOLVE':
+            "From Blender to DaVinci Resolve."
+            seconds = 60
+            minutes = 60
+            frame += -1 + self._fps * seconds * minutes
+
+        else:
+            assert False, "Style :{0} not supported".format(style)
+
+        return bpy.utils.smpte_from_frame(
+            frame, fps=self._fps, fps_base=self._fps_base)
 
 
 class ImageSequenceStrip(Strip):
     _channel_type = 'V'
 
     def _process_strip(self, strip):
+        frame_start = strip.frame_start
         frame_offset_start = strip.frame_offset_start
         frame_final_duration = strip.frame_final_duration
 
@@ -58,28 +80,28 @@ class ImageSequenceStrip(Strip):
         self._edit_out = self._edit_in + frame_final_duration
 
         directories = strip.directory.split(os.path.sep)
-        assert(len(directories) > 1 and directories[-1])
-        self._reel_name = directories[-1]
+        assert(len(directories) > 1 and directories[-2])
+        self._reel_name = directories[-2]
 
 
 # ############################################################
 # Main Exporting Routine
 # ############################################################
 
-def export(filepath, fps, strips):
+def export(filepath, fps, fps_base, strips):
     """Create a new file"""
     """
     It's a bit tricky because we need to sort the strips in order
     and extract their reel number
     """
-    order_strips = [ImageSequenceStrip(s, i) for s, i in enumerate(
+    order_strips = [ImageSequenceStrip(i, s, fps, fps_base) for i, s in enumerate(
         sorted(strips, key=lambda s: s.frame_start + s.frame_offset_start))]
 
     with open(filepath, 'w') as f:
         f.write("TITLE: Agent 327\n")
 
         for strip in order_strips:
-            f.write(strip.to_edl)
+            f.write(strip.to_edl())
 
 
 # ############################################################
@@ -102,7 +124,12 @@ class SEQUENCER_OT_EDLExport(Operator, ExportHelper):
     def execute(self, context):
         """Build EDL file"""
         scene = context.scene
-        fps = scene.render.fps / scene.render.fps_base
+        fps = scene.render.fps
+        fps_base = scene.render.fps_base
+
+        if not scene.sequence_editor:
+            self.report({'ERROR'}, "Scene has no video sequence")
+            return {'CANCELLED'}
 
         channels = {}
         for strip in scene.sequence_editor.sequences:
@@ -117,7 +144,7 @@ class SEQUENCER_OT_EDLExport(Operator, ExportHelper):
 
         for v_index, strips in channels.items():
             filepath = self.get_filepath(v_index)
-            export(filename, self.fps, strips)
+            export(filepath, fps, fps_base, strips)
 
         return {'FINISHED'}
 
@@ -131,7 +158,7 @@ class SEQUENCER_OT_EDLExport(Operator, ExportHelper):
         if basename.endswith(".edl"):
             basename = basename[:-4]
 
-        return os.path.join(dirname, "{0}-V{1}.edl".formt(basename, v_index))
+        return os.path.join(dirname, "{0}-V{1}.edl".format(basename, v_index))
 
 
 # ############################################################

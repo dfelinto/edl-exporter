8925c49873f8d3037a11bbf69d7aa40f5727c991
Dalai Felinto <dfelinto@gmail.com>
2017-05-08 00:10:55 +0200

    TIF to EXR converter, Linux

---
+++ exr_to_tif_converter/convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Convert the .exr from the sequencer to .tif files"
+
+BLENDER=/home/guest/src/blender/versions/blender-2.78a/blender
+PYTHON_SCRIPT=/shared/software/edl-exporter/exr_to_tif_converter/exr_to_tif_converter.py
+EDIT_FILE=/render/agent327/export/tools/edit.blend
+CHANNEL=31
+OUTPUT=/render/agent327/frames_grade
+
+export OCIO=/shared/software/filmic-blender/config.ocio
+$BLENDER -b $EDIT_FILE -P $PYTHON_SCRIPT -- $CHANNEL $OUTPUT
